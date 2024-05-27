c23f76884e7531ddd997ead3108d3e0036a96598
chilachan <penpengin9336@gmail.com>
2024-05-27 19:20:28 +0900

    dadadaa

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,7 +1,3 @@
-import Head from 'next/head'
-import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
